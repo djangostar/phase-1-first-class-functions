d237d3a322ec8995d18537a9a99ca148c5d76aba
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2023-12-14 17:32:21 -0800

    Redone with Comments

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const receivesAFunction = (callback) => callback();
-
-const returnsANamedFunction = () => {
-  return function namedFunction() {
-    console.log("Returned");
-  };
+// take a callback function as an argument
+const receivesAFunction = (callback) => {
+  // call the callback function
+  callback();
+  // it doesn't matter what this function returns, so long as it calls the callback function
 };
-
+// take no arguments
+const returnsANamedFunction = () => {
+  // return a named function
+  return function Ace() {};
+};
+// take no arguments
 const returnsAnAnonymousFunction = () => {
-  return function () {
-    console.log("Anonymous function");
-  };
+  return function () {};
 };
